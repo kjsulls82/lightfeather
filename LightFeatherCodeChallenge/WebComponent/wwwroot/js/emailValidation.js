c46f272a76efac4987677ab3d074943ea40fd96b
Kyle Sullivan <kjsulls@gmail.com>
2020-04-19 12:26:02 -0500

    finished webcomponent

---
+++ LightFeatherCodeChallenge/WebComponent/wwwroot/js/emailValidation.js
@@ -0,0 +1,14 @@
+﻿var emailValidation = emailValidation || function () {
+
+    const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gi;
+
+    function exec(str) {
+
+        var result = pattern.test(str);
+        return result;
+    }
+
+    return {
+        exec: exec
+    };
+};
